3df1f907fa5b6ffb1d0e9d1e09550f391b099c6e
Micheal Olan <mikkysar@gmail.com>
2021-03-10 20:14:15 +0000

    edits

--- Display_Reminders.py
+++ Display_Reminders.py
@@ -11,19 +11,23 @@ database = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
 cursor = database.cursor()
 
 
+def delete_reminders():
+    pass
+
+
 def main(**kwargs):
     # current_user = kwargs.get('current_user', None)
-    current_user = 'cryola'
+    current_user = 'Mikko123'
     SG.theme('DarkBlue1')
     entry_field_size = (30, 5)
 
     button_font = ('Sans', 15)
     text_font = ('Sans', 15)
-    #change start date to last run
-    #update table when user creates new reminder
-    #add delete icon
+    # change start date to last run
+    # update table when user creates new reminder
+    # add delete icon
     reminders = cursor.execute("Select Title, Message, Frequency_type, CONVERT(varchar(10),Start_date, 103) AS 'Start "
-                               "Date', Reminder_time from dbo.Reminders "
+                               "Date', Reminder_time, id from dbo.Reminders "
                                "as r INNER JOIN dbo.Reminder_users as ru on r.User_id = "
                                "ru.UserID WHERE ru.Username = ? ", current_user).fetchall()
     # header for the table
@@ -35,14 +39,15 @@ def main(**kwargs):
         [SG.Button('Logout', font=button_font),
          SG.Text('Reminders', font=('Sans', 30), size=(1000, 1), justification='c')],
         [SG.Text('')],
-        [SG.Table(values=list(reminder_list), headings=header_list, display_row_numbers=True, justification='c',
-                  auto_size_columns=True,row_height=25 ,max_col_width=10, key='-TABLE-')],
-
-        [SG.Text('')],
+        [SG.Table(values=reminder_list, headings=header_list, display_row_numbers=True, justification='c',
+                  auto_size_columns=True, row_height=25, max_col_width=10, enable_events=True, key='-TABLE-')],
+        [SG.T('')],
+        [SG.Button('Delete Reminder', font=text_font, size=(15, 1))],
+        [SG.T('')],
         [SG.Button('Create New', font=text_font, size=(15, 1))]
 
     ]
-    window = SG.Window("Reminders", layout, size=(500, 500), element_justification='C')
+    window = SG.Window("Reminders", layout, size=(500, 515), element_justification='C')
 
     while True:
         event, values = window.read()
@@ -56,6 +61,9 @@ def main(**kwargs):
         if event == 'Logout':
             window.close()
             MS.main()
+
+        if event == 'Delete Reminder':
+            delete_reminders()
 
 
 if __name__ == '__main__':
