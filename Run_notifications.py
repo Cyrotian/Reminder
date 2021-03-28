36230d78f024fd90bddba4e0bcdb4e98d44b1b5d
Micheal Olan <mikkysar@gmail.com>
2021-03-28 23:07:39 +0100

    changes

--- Run_notifications.py
+++ Run_notifications.py
@@ -26,32 +26,30 @@ cursor = database.cursor()
 
 
 def update_lastrun(reminder_id):
-    pass
+    cursor.execute("Update Reminders SET last_run = Getdate() where id = ?", reminder_id)
+    cursor.commit()
 
 
 def get_runtime(title, message, freq_type, freq_int, start_date, last_run, reminder_time, id):
     # getting the time in "H:M" format
-    print(title)
     current_date = dt.datetime.now().date()
     current_time = dt.datetime.now().time().strftime("%H:%M")
-
-    if current_time > str(reminder_time):
-        pass
-        # print(current_time + '' + reminder_time)
+    current_run = last_run + dt.timedelta(days=freq_int)
 
     # if the reminder has run previously then use the last_run date
     if last_run is not None:
         # adding frequency interval to the last run to determine if this reminder is supposed to run today
-        current_run = last_run + dt.timedelta(days=freq_int)
         # checking if the date and the time match up to show the notification
         if current_run == current_date and current_time == str(reminder_time):
             run_notification(title, message)
+            update_lastrun(id)
     else:
         # if the reminder hasn't run before than use the start date
         if start_date == current_date and current_time == str(reminder_time):
             run_notification(title, message)
+            update_lastrun(id)
 
-    if start_date == dt.datetime.now().date():
+    if start_date or current_run == dt.datetime.now().date():
         # if current_time == str(reminder_time):
         run_notification(title, message)
 
@@ -61,7 +59,7 @@ def run_notification(title, message):
     toast.show_toast(
         title=title,
         msg=message,
-        duration=5,
+        duration=7,
         icon_path='.\\reminder_icon.ico'
     )
     # notification.notify(
@@ -78,7 +76,7 @@ def main():
     active_user_notifications = cursor.execute("Select id, Title, Message, frequency_type,frequency_interval, "
                                                "start_date, Last_run, Reminder_time from dbo.reminders where "
                                                "active_user = 1 and ((Start_date = cast(GETDATE() as date) or ("
-                                               "DATEADD(DD, Frequency_interval, Last_run)= GETDATE())))").fetchall()
+                                               "DATEADD(DD, Frequency_interval, Last_run)= cast(GETDATE()as date))))").fetchall()
 
     for i in range(len(active_user_notifications)):
         notification_values = []
@@ -89,6 +87,7 @@ def main():
             else:
                 notification_values.append(active_user_notifications[i][j])
         # assigning values in notifications values into variables starting at position 1
+
         title, message, freq_type, freq_int, start_date, last_run, reminder_time = [notification_values[i] for i in
                                                                                     range(1, len(notification_values))]
 
