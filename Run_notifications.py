3243006849a09f36a244fe276b9bc61406ce1f7c
Micheal Olanegan <mikkysar@gmail.com>
2021-03-31 14:32:14 +0100

    Update Run_notifications.py

--- Run_notifications.py
+++ Run_notifications.py
@@ -4,18 +4,7 @@ import pyodbc
 import datetime as dt
 import time
 
-# get list of reminders for active users
-# check the frequency type
-# if this equals once- check the reminder time and start date
-# if start date is in the past ignore
-# else check the last run date
-# if the last run time is null, check if adding the start_date to the interval equals today
-# if it does equal today, check the reminder time
-# if it doesn't equal today ignore
-# if the reminder time is past the current time(pc was turn on later so reminder didn't run)
-# and it's not frequency type - once , update last run to equal today.
-# if the reminder time is still a time in the future - add title and message to list
-# use the time module to check if the time matches,if it does pass title and message to notification, show notification
+
 # add leeway to time + 3 minutes (sleep  for 3 mintues) time.sleep(seconds)
 database = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                           'Server= MICHAEL-PC\SQLSSIS;'
